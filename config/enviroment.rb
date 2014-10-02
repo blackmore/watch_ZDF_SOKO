b4238eb734ba632b6c32f7f91b18803695e2f82a
Nigel Blackmore <nigel@blackmore.de>
2014-10-02 14:20:51 +0200

    conf for production
    added correct paths for production

--- config/enviroment.rb
+++ config/enviroment.rb
@@ -4,16 +4,19 @@ ROOT = File.expand_path('../../', __FILE__)
 # TEST PATHS - COMMENT OUT IN PRODUCTION
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
-SOURCE_PATH = "#{ROOT}/test_folders/"
-PROCESSED_PATH = "#{ROOT}/test_folders/processed"
-TARGET_PATH = "#{ROOT}/test_folders/target_ezt_watch"
+# SOURCE_PATH = "#{ROOT}/test_folders/"
+# PROCESSED_PATH = "#{ROOT}/test_folders/processed"
+# TARGET_PATH = "#{ROOT}/test_folders/target_ezt_watch"
+# 
 
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 # TEST PATHS
 # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 
 # set this ti the path of the folder you wish to watch
-# SOURCE_PATH = "/Users/nigel/Documents/Code2/media/mediaserver/advantage_2_midi"
+SOURCE_PATH = "/media/mediaserver/watchers/ZDF_txt_scripts_2_ezt/SOKO_scripts"
 
 # set this to the path where to place the sources once finished
-# PROCESSED_PATH = "#{SOURCE_PATH}/processed"
+PROCESSED_PATH = "#{SOURCE_PATH}/processed"
+
+TARGET_PATH = "/media/mediaserver/workings/ZDF_Script_parser"
